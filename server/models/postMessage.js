20aced676d1d9ccba7cb7916b4d5e7306e19d8e7
Steve-07 <55524682+Steve-07@users.noreply.github.com>
2022-11-15 13:51:39 -0800

    front and back end connected. submitting form adds to database

---
+++ server/models/postMessage.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    submittor: String,
+    description: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const postMessage = mongoose.model('postMessage', postSchema);
+
+export default postMessage;
