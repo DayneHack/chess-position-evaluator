56a8149cb563ad8414acb20c4591bd08b58a7806
Steve-07 <55524682+Steve-07@users.noreply.github.com>
2022-12-09 16:10:02 -0800

    project ready to deploy

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Running on port ${PORT}`)))
